91f3e26bcc8779ee8979f267ff4d4ce978d0dc44
Graham Hall <graham@ghall.dev>
2024-02-04 21:32:06 -0500

    pagination

--- src/utils/db/getAllMembers.ts
+++ src/utils/db/getAllMembers.ts
@@ -1,15 +1,35 @@
 import { getKnex } from '@knex/db';
 import type { Member } from '../../types';
 
-export default async () => {
+export type Response = {
+  data: Member[];
+  count: number;
+  pages: number[];
+};
+
+export default async (page: number = 1) => {
   const knex = getKnex();
-  let memberships: Member[];
+  let memberships: Response = {
+    data: [],
+    count: 0,
+    pages: [],
+  };
+
+  const limit = 10;
+  const offset = (page - 1) * limit;
 
   try {
-    memberships = await knex('member');
-    return memberships;
+    const [{ count }] = await knex('member').count();
+
+    memberships.data = await knex('member').offset(offset).limit(limit);
+    memberships.count = Number(count);
+    memberships.pages = Array.from(
+      { length: memberships.count / limit / 1 + 1 },
+      (value, index) => 1 + index * 1,
+    );
   } catch (error) {
     console.error(error);
-    return [];
   }
+
+  return memberships;
 };
